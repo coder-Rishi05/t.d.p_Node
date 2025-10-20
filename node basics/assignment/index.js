25c706e6cfeb591e3f317b4f63ee329006c6740c
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-10-20 19:49:45 +0530

    completed assingment - 1

--- node basics/assignment/index.js
+++ node basics/assignment/index.js
@@ -1,29 +1,32 @@
-const createServer = require("node:http");
-
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
 const hostname = "127.0.0.1";
 const port = 8080;
 
-// const server = createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader("Content-Type", "text/html");
-//   res.end("assignment/index.html");
-// });
+const server = http.createServer((req, res) => {
+  let filePath = "";
 
-var http = require('http'),
-    fs = require('fs');
-
-
-fs.readFile('./index.html', function (err, html) {
+  if (req.url === "/" || req.url === "/index") {
+    filePath = path.join(__dirname, "index.html");
+  } else if (req.url === "/about") {
+    filePath = path.join(__dirname, "about.html");
+  } else if (req.url === "/contact-me") {
+    filePath = path.join(__dirname, "contact-me.html");
+  } else {
+    filePath = path.join(__dirname, "404.html");
+  }
+  fs.readFile(filePath, (err, content) => {
     if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(8000);
+      res.writeHead(500);
+      res.end("Error loading page.");
+    } else {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(content);
+    }
+  });
 });
 
-// server.listen(port, hostname, () => {
-//   console.log(`Server running at http://${hostname}:${port}/`);
-// });
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
